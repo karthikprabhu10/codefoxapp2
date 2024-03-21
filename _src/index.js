2c41f85b3c0bb8c227e15cff44d138eee5d3923f
K4x <108575187+karthikprabhu10@users.noreply.github.com>
2024-03-21 22:51:13 +0530

    Update index.js

--- _src/index.js
+++ _src/index.js
@@ -6,7 +6,7 @@ const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
 import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: 'CodeFox', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
+FloatingPrompt({ name: 'CodeFox', url: '', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
 import Darkmode from 'darkmode-js'
